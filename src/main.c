f771cc1b917c5026dfc3b0f22f213a0bb7e9cbb4
staugi <ugnius@dev.stasaitis.me>
2025-06-25 10:15:30 +0300

    Syslog

--- src/main.c
+++ src/main.c
@@ -1,10 +1,26 @@
 #include <libubus.h>
+#include <sys/syslog.h>
+#include <syslog.h>
 #include "ubus.h"
+
+#define SYSLOG_OPTIONS LOG_PID | LOG_NDELAY
 
 static struct ubus_context *g_ubus_context;
 
 int main(void) {
-    ubus_init(&g_ubus_context);
+    openlog(NULL, SYSLOG_OPTIONS, LOG_LOCAL0);
+
+    switch (ubus_init(&g_ubus_context)) {
+        case UBUS_RESULT_ERROR_CONNECTION_FAILED:
+            syslog(LOG_ERR, "Failed to connect to ubus.");
+            break; 
+        case UBUS_RESULT_ERROR_INIT_FAILED:
+            syslog(LOG_ERR, "Ubus initialization failed.");
+            break;
+        case UBUS_RESULT_OK:
+            syslog(LOG_INFO, "Esp communication daemon started successfully.");
+            break;
+    }
     uloop_run();
     ubus_deinit(g_ubus_context);
 
